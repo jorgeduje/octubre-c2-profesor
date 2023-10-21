0de1ee9a541ca91866048ffda5594bce2549a9a8
jorgeduje <jorgeduje4@gmail.com>
2023-10-20 21:58:19 -0300

    se trabaja con variables y tipos de datos

--- prueba.js
+++
@@ -1,7 +0,0 @@
-
-let nombre = "Jorge";
-let x = "modificado"
-
-let y = "rama2"
-
-console.log(nombre);
---
+++ variables.js
@@ -0,0 +1,84 @@
+
+// VARIABLES --> cajas que guardan algo
+
+// let - const
+
+// console.log( edad )
+
+let edad = 30
+
+let nombre = "Pepito"
+
+console.log( nombre )
+
+console.log(edad)
+
+const apellido = "Perez"
+
+console.log(apellido)
+
+console.log(edad )
+
+edad = 31
+
+console.log(edad)
+
+nombre = "juan"
+
+// apellido = "lopez"
+
+let nombredeusuario = "juan carlos"
+let nombreDeUsuario = "juan carlos"
+let nombre_de_usuario = "juan carlos"
+
+
+// TIPOS DE DATOS
+
+// strings ---> cadenas de texto
+
+let frase =  "Hoy es un lindo dia!"
+let frase2 =  'Hoy es un lindo dia!'
+
+let frase3 = 'hola "pepe" como estas?'
+
+// numbers ---> numeros
+
+let precio = 500 //--> float o integer
+
+let telefono = "41231124"
+
+console.log(typeof precio)
+console.log(typeof telefono)
+
+// booleans --> booleanos --> true y false
+
+let elUsuarioEstaLogueado = true // false
+
+console.log(typeof elUsuarioEstaLogueado )
+
+// undefined --> indefinido
+// null --> nulo 
+// NaN  --> not a number
+
+let nombreUsuario = null
+
+let edad2 = undefined
+
+let a = 15 
+let b = "hola pepito como estas?"
+let x = a * b
+console.log(x)
+
+
+let y
+
+let r = "hola"
+
+console.log(y)
+
+a = "pepe"
+
+console.log(a)
+
+
+// OPERADORES
